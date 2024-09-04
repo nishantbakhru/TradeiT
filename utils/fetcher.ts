0e98f39860ecda536f218ae8be235ca6fc4ac1cc
Nash Back <nishant_bakhru@alumni.brown.edu>
2024-09-04 03:13:49 -0400

    removed old scaffolding

--- utils/fetcher.ts
+++
@@ -1,7 +0,0 @@
-export async function fetcher(url: string, options: RequestInit = {}) {
-    const response = await fetch(url, options);
-    if (!response.ok) {
-      throw new Error(`Failed to fetch: ${response.statusText}`);
-    }
-    return response.json();
-  }
